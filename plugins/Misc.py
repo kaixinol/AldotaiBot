fd97dcf52cb946772b3d33da79fc67018d3a7983
开普以尔 <keiplyer@gmail.com>
2023-11-07 04:08:52 +0800

    bugfix

--- plugins/Misc.py
+++ plugins/Misc.py
@@ -1,4 +1,4 @@
-from random import choice, randint
+from random import choice
 
 from graia.ariadne.app import Ariadne
 from graia.saya import Channel
@@ -15,25 +15,26 @@ spider = Session("Misc")
 
 
 @channel.use(SchedulerSchema(timers.every_custom_hours(1)))
-async def auto_get_cookie(app: Ariadne):
-    syj_data = setting["plugin"]["ShouYunJi"]
-    if (
-        await spider.get_json(
-            "https://cloud.foxtail.cn/api/account/state",
-            cookie=setting["plugin"]["ShouYunJi"]["cookie"],
-        )
-    )["code"] != "11100":
-        logger.warning("Cookie已经丢失，正在尝试重新获取中……")
-        setting["plugin"]["ShouYunJi"]["cookie"] = await spider.get_cookie(
-            "https://cloud.foxtail.cn/api/account/login",
-            kw={
-                "account": syj_data["account"],
-                "password": syj_data["password"],
-                "model": "1",
-                "token": syj_data["token"],
-            },
-        )
-        logger.info(setting["plugin"]["ShouYunJi"]["cookie"])
+async def auto_get_cookie():
+    if not setting["plugin"]["ShouYunJi"]["disabled"]:
+        syj_data = setting["plugin"]["ShouYunJi"]
+        if (
+                await spider.get_json(
+                    "https://cloud.foxtail.cn/api/account/state",
+                    cookie=setting["plugin"]["ShouYunJi"]["cookie"],
+                )
+        )["code"] != "11100":
+            logger.warning("Cookie已经丢失，正在尝试重新获取中……")
+            setting["plugin"]["ShouYunJi"]["cookie"] = await spider.get_cookie(
+                "https://cloud.foxtail.cn/api/account/login",
+                kw={
+                    "account": syj_data["account"],
+                    "password": syj_data["password"],
+                    "model": "1",
+                    "token": syj_data["token"],
+                },
+            )
+            logger.info(setting["plugin"]["ShouYunJi"]["cookie"])
 
 
 @channel.use(SchedulerSchema(timers.every_custom_hours(12)))
